0b9867ace87162d87c8b0a93e13087479a81d067
Soren Zaiser <soren.zaiser@gmail.com>
2019-05-15 12:05:39 -0400

    Initial Commit
    Code Base from clonemeagain/osticket-plugin-mentioner

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php return array(
+ 'id' => 'clonemeagain:mentioner', # notrans
+ 'version' => '1.2',
+ 'name' => 'Mentioner',
+ 'author' => 'clonemeagain@gmail.com',
+ 'description' => 'Notices @mentions and those names as collaborators.',
+ 'url' => 'https://github.com/clonemeagain/osticket-plugin-mentioner',
+ 'plugin' => 'class.MentionerPlugin.php:MentionerPlugin'
+);
